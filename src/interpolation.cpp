da5b333a93cec208e5462aff85da71c2f0575d07
George Kiriy <gkiriy@smedialink.com>
2016-05-29 23:30:02 +0300

    Small refactoring

--- src/interpolation.cpp
+++ src/interpolation.cpp
@@ -6,10 +6,11 @@
 //#include "CFunction.h"
 
 using namespace std;
+using namespace TCLAP;
 
-vector <Point> getInputArray(TCLAP::ValueArg<string> &expression, TCLAP::ValueArg<string> &inputPath);
+vector <Point> getInputArray(ValueArg<string> &expression, ValueArg<string> &inputPath);
 
-void interpolate(TCLAP::ValueArg<string> &type, vector <Point> &input);
+void interpolate(ValueArg<string> &type, vector <Point> &input);
 
 map<string, CInerpolator *> initializeInterpolators();
 
@@ -18,18 +19,18 @@ void drawLine(const string &name, vector <Point> &data);
 void interpolateAndDraw(const string &name, CInerpolator *interpolator, vector <Point> &input);
 
 int main(int argc, const char *argv[]) {
-    TCLAP::CmdLine cmd("Command description message", ' ', "0.9");
+    CmdLine cmd("Command description message", ' ', "0.9");
 
     // Interpolate existing function or use file as input
-    TCLAP::ValueArg<std::string> expressionString("e", "expr", "Function for interpolation", false, "x^2", "string");
-    TCLAP::ValueArg<std::string> inputFile("i", "input", "Input file", false, "", "string");
+    ValueArg<string> expressionString("e", "expr", "Function for interpolation", false, "x^2", "string");
+    ValueArg<string> inputFile("i", "input", "Input file", false, "", "string");
     cmd.xorAdd(expressionString, inputFile);
 
     // Choose interpolation type out of supported
     vector<string> allowedTypes;
     allowedTypes.push_back("lagrange");
-    TCLAP::ValuesConstraint<string> constraint = TCLAP::ValuesConstraint<string>(allowedTypes);
-    TCLAP::ValueArg<std::string> type("t", "type", "Interpolation type", false, "", &constraint, cmd);
+    ValuesConstraint<string> constraint = ValuesConstraint<string>(allowedTypes);
+    ValueArg<string> type("t", "type", "Interpolation type", false, "", &constraint, cmd);
 
     // Parse arguments
     cmd.parse(argc, argv);
@@ -43,7 +44,7 @@ int main(int argc, const char *argv[]) {
     return EXIT_SUCCESS;
 }
 
-void interpolate(TCLAP::ValueArg<string> &type, vector<Point> &input) {
+void interpolate(ValueArg<string> &type, vector<Point> &input) {
     map<string, CInerpolator *> interpolators = initializeInterpolators();
 
     if (type.isSet()) {
@@ -71,7 +72,7 @@ map<string, CInerpolator *> initializeInterpolators() {
     return interMap;
 }
 
-vector<Point> getInputArray(TCLAP::ValueArg<string> &expression, TCLAP::ValueArg<string> &inputPath) {
+vector<Point> getInputArray(ValueArg<string> &expression, ValueArg<string> &inputPath) {
     // TODO: Finish
     vector<Point> input;
     input.push_back(Point(0, 0));
